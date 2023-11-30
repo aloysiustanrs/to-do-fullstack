e64548dc209ff9a4a99c2f0017ffde327b82d04c
Aloysius Tan <aloysiustanrs@gmail.com>
2023-11-30 21:14:40 +0800

    able to run frontend backend db container w docker
    however not able to find db 'user' in postgresql db

--- backend/db/index.js
+++ backend/db/index.js
@@ -3,12 +3,34 @@ const { dbUser, dbPassword } = require('../config');
 
 const db = new Client({
   user: dbUser,
-  host: 'localhost',
+  host: 'postgres',
   database: 'postgres',
   password: dbPassword,
   port: 5432,
 });
 
-db.connect();
+async function connectWithRetry(retries) {
+  try {
+    await db.connect();
+    console.log('Connected to the database!');
+  } catch (err) {
+    console.error(err);
+    console.log(`Retries left: ${retries}`);
+    if (retries > 0) {
+      // Wait 5 seconds before each retry
+      await new Promise(resolve => setTimeout(resolve, 5000));
+      // Retry with reduced number of retries
+      await connectWithRetry(retries - 1);
+    } else {
+      console.error('Max retries reached. Unable to connect to the database.');
+      // You might want to handle this case, such as throwing an error or exiting the application
+    }
+  }
+}
+
+// Start the connection process with the maximum number of retries
+connectWithRetry(5);
+
+
 
 module.exports = db;
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const db = require('../db');
-const { jwtSecret, jwtExpiresIn } = require('../config');
+const { jwtExpiresIn } = require('../config');
 
 
 // Register a new user and generate JWT token
