dfca1bff88660483f4423c88b69df1f04f89e71d
Aloysius Tan <aloysiustanrs@gmail.com>
2024-03-20 13:23:46 +0800

    Testing github action workflow

--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -3,6 +3,6 @@ import { useTaskContext } from "../context/TaskContext";
 const Test = () => {
   const { testString } = useTaskContext();
 
-  return <div>TEST 1132 03202024 "{testString}"</div>;
+  return <div>TEST 0123 03202024 "{testString}"</div>;
 };
 export default Test;
